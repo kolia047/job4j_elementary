fec82767d61459e23c189df9e37d06c21e5a330f
kolia047 <kolia047@mail.ru>
2021-11-01 14:16:16 +0600

    4.1. Разбор первой программы. [#159033 #221370]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -2,8 +2,7 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
-        System.out.println("Николай Первушин");
+        System.out.println("Первушин Николай");
         System.out.println("02.09.2021");
-
     }
 }
