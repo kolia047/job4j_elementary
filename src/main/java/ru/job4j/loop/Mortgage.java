9282a413e618c54d82a38ee84edbe53386634fe0
Николай <kolia047@mail.ru>
2021-12-02 22:30:10 +0600

    5.7. Ипотека [#156315 #235238]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,10 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 1;
-        while (amount > 0){
+        while (amount > 0) {
             percent = amount * percent * 0.01;
             amount = (amount + percent) - salary;
-            if(amount > 0){
+            if (amount > 0) {
                 year++;
             }
         }
