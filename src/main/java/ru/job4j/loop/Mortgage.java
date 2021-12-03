a042a086d96590c3bf8aec6099c04ac15b14e26b
kolia047 <kolia047@mail.ru>
2021-12-03 15:54:50 +0600

    5.7. Ипотека [#156315 #235238]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,8 +4,7 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            percent = amount * percent * 0.01;
-            amount = amount + percent - salary;
+           amount += amount * percent * 0.01 - salary;
             year++;
         }
         return year;
