2cabe37beb0289ce04708d89547a5b2cff9b70b5
Николай <kolia047@mail.ru>
2021-12-07 22:54:00 +0600

    222

--- src/main/java/ru/job4j/loop/DegreeLoop.java
+++ src/main/java/ru/job4j/loop/DegreeLoop.java
@@ -2,18 +2,17 @@ package ru.job4j.loop;
 
 public class DegreeLoop {
     public static int calculate(int a, int n) {
-        int i;
-        int sum=a;
-        for(i=1; i <= n; i++){
-           sum *= sum;
+        int result = 1;
+        for(int i=1; i <= n; i++){
+           result = result * a;
 
         }
 
-        return sum;
+        return result;
     }
 
     public static void main(String[] args) {
-        int a = DegreeLoop.calculate(2,2);
+        int a = DegreeLoop.calculate(3,2);
         System.out.println(a);
     }
 }
