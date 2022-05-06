6e3cc99de5cf59650dab65a34d29dde001afc185
Николай <kolia047@mail.ru>
2022-05-06 14:44:01 +0600

    0.7. Максимум из первого и последнего элемента массива

---
+++ src/main/java/ru/job4j/array/MaxFirstOrLastElement.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class MaxFirstOrLastElement {
+
+    public static int getMaxValue(int[] array) {
+
+        if (array[0] > array.length){
+            return array[0];
+        }
+        return array.length;
+    }
+}
---
+++ src/test/java/ru/job4j/array/MaxFirstOrLastElementTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+public class MaxFirstOrLastElementTest {
+
+    @Test
+    public void getMaxValueFirstElement() {
+        int[] in = {5, 4, 3, 2, 1};
+        int expected = 5;
+        int rsl = MaxFirstOrLastElement.getMaxValue(in);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void getMaxValueLastElement() {
+        int[] in = {1, 2, 3, 4, 5};
+        int expected = 5;
+        int rsl = MaxFirstOrLastElement.getMaxValue(in);
+        assertThat(rsl, is(expected));
+    }
+}
