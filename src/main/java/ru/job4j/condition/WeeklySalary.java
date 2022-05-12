7a55db64d34170a4b8c4f04bbcba883582afe64a
Николай <kolia047@mail.ru>
2022-05-12 13:56:59 +0600

    1.6. Еженедельная зарплата

---
+++ src/main/java/ru/job4j/condition/WeeklySalary.java
@@ -0,0 +1,31 @@
+package ru.job4j.condition;
+
+public class WeeklySalary {
+    public static int calculate(int[] hours) {
+        int summa = 0;
+        int summaOvertime = 0;
+        for (int i = 0; i < 5; i++) {
+            if (hours[i] > 8) {
+                summaOvertime = summaOvertime + ((hours[i] - 8) * 15);
+                summaOvertime = summaOvertime + 80;
+
+            } else if (hours[i] <= 8 ) {
+                summa = summa + (hours[i] * 10);
+            }
+
+        }
+        for (int i = 5; i < 7; i++) {
+            if (hours[i] > 8) {
+                summaOvertime = summaOvertime + ((hours[i] - 8) * 30);
+                summaOvertime = summaOvertime + 160;
+
+            } else if (hours[i] <= 8 ) {
+                summa = summa + (hours[i] * 20);
+            }
+
+        }
+
+        return summa + summaOvertime;
+    }
+}
+
