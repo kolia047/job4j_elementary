f72b513435bbeeaf57637e5294afe728d1f185a0
Николай <kolia047@mail.ru>
2022-05-26 14:44:41 +0600

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,50 +8,38 @@ import static org.junit.Assert.*;
 public class PointTest {
 
     @Test
-    public void when23to46then3dot6() {
-        /* входные данные */
-        int x1 = 2;
-        int y1 = 3;
-        int x2 = 4;
-        int y2 = 6;
-        /* ожидаемое значение */
-        double expected = 3.6;
-        /* вызов проверяемого метода*/
-        double out = Point.distance(x1, y1, x2, y2);
-        /* сравнения */
-        Assert.assertEquals(expected, out, 0.1);
-
+    public void when00to20then2() {
+        double expected = 2.0;
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
-    public void when12to34then2dot8() {
-        /* входные данные */
-        int x1 = 1;
-        int y1 = 2;
-        int x2 = 3;
-        int y2 = 4;
-        /* ожидаемое значение */
-        double expected = 2.8;
-        /* вызов проверяемого метода*/
-        double out = Point.distance(x1, y1, x2, y2);
-        /* сравнения */
-        Assert.assertEquals(expected, out, 0.1);
-
+    public void when12to69then8dot60() {
+        double expected = 8.60;
+        Point a = new Point(1, 2);
+        Point b = new Point(6, 9);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
-    public void when58to62then6() {
-        /* входные данные */
-        int x1 = 5;
-        int y1 = 8;
-        int x2 = 6;
-        int y2 = 2;
-        /* ожидаемое значение */
-        double expected = 6;
-        /* вызов проверяемого метода*/
-        double out = Point.distance(x1, y1, x2, y2);
-        /* сравнения */
-        Assert.assertEquals(expected, out, 0.1);
+    public void when58to15then5() {
+        double expected = 5.0;
+        Point a = new Point(5, 8);
+        Point b = new Point(1, 5);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
+    }
 
+    @Test
+    public void when21to41then2() {
+        double expected = 2.0;
+        Point a = new Point(2, 1);
+        Point b = new Point(4, 1);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 }
