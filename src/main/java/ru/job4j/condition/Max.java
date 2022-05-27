6ebd09b8789546f8d551169888ee7d86ec2df074
Николай <kolia047@mail.ru>
2022-05-27 14:12:12 +0600

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,12 +2,19 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
+        return left > right ? left : right;
+    }
+
+    public static int max(int left, int second, int third) {
+        return max(left, max(second, third));
+    }
+
+    public static int max(int left, int second, int third, int four) {
+        return max(left, max(second, third, four));
     }
 
     public static void main(String[] args) {
-        int end = Max.max(24, 45);
+        int end = Max.max(24, 45, 53, 112);
         System.out.println(end);
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -1,34 +1,38 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class MaxTest {
 
     @Test
-    public void when24To45Then45() {
-        int left = 24;
-        int right = 45;
-        int result = Max.max(left, right);
+    public void when24and45Then45() {
+        int first = 24;
+        int second = 45;
         int expected = 45;
-        Assert.assertEquals(result, expected);
+        int out = Max.max(first, second);
+        assertEquals(expected, out);
     }
 
     @Test
-    public void when18To11Then18() {
-        int left = 18;
-        int right = 11;
-        int result = Max.max(left, right);
-        int expected = 18;
-        Assert.assertEquals(result, expected);
+    public void when24and45and53Then53() {
+        int first = 24;
+        int second = 45;
+        int third = 53;
+        int expected = 53;
+        int out = Max.max(first, second, third);
+        assertEquals(expected, out);
     }
 
     @Test
-    public void when21To21Then21() {
-        int left = 21;
-        int right = 21;
-        int result = Max.max(left, right);
-        int expected = 21;
-        Assert.assertEquals(result, expected);
+    public void when25and12and2548and1256Then2548() {
+        int first = 25;
+        int second = 12;
+        int third = 2548;
+        int four = 1256;
+        int expected = 2548;
+        int out = Max.max(first, second, third, four);
+        assertEquals(expected, out);
     }
 }
